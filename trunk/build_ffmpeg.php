1cfb423ad4797f53eb5a0e36fb9d5231e4171096
mapopa <mapopa@cd4997d5-a750-0410-a0e3-67582cbb3c3b>
2008-06-30 09:00:24 +0000

    added passthru test

--- trunk/build_ffmpeg.php
+++ trunk/build_ffmpeg.php
@@ -1,5 +1,9 @@
 #!/usr/bin/php
+<?php
+# requirments apt-get install php5-cli before using this script :)
 
-#requirments apt-get install php5-cli before using this script :)
+passthru ('sudo apt-get install ruby subversion gcc git-core automake unzip libogg-dev libvorbis-dev');
+
+?>
 
 
