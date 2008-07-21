090630125432d16e99ba57b25e1c0eb2b1992dfe
mapopa <mapopa@cd4997d5-a750-0410-a0e3-67582cbb3c3b>
2008-07-21 10:03:18 +0000

    added small guide

--- git-snapshot-script.php
+++ git-snapshot-script.php
@@ -1,5 +1,18 @@
 #!/usr/bin/php
 <?php
+/*See how to use it http://www.jukie.net/~bart/blog/20070312134706
+install git
+apt-get install git-core
+cd /etc
+git init-db
+chmod og-rwx .git
+commit the current state
+git add .
+git commit -a -m"initial import"
+
+then you can add an daily snapshot script or an after upgrade one (after dist-upgrade)
+./git-snapshot-script.php
+*/
 
 $today = date("D M j G:i:s T Y");
 print "/etc Backup for $today";
