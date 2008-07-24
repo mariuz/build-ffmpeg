5235442826ca9e193968bc4c608e5ea75b5d629d
mapopa <mapopa@cd4997d5-a750-0410-a0e3-67582cbb3c3b>
2008-07-24 15:56:06 +0000

    added vlc from git script

---
+++ trunk/build_vlc_git.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+sudo rm -rf vlc
+git clone git://git.videolan.org/
+cd vlc
+sudo ./bootstrap
+./configure --mandir=/share/man --build=x86_64-linux-gnu --enable-maintaner-mode --enable-release --prefix=/usr --enable-libtool --enable-fast-install --disable-update-check --disable-gnome --disable-gtk --disable-familiar --disable-fb --enable-ggi --enable-sdl --enable-esd --enable-mad --enable-arts --enable-alsa --enable-jack --enable-pulse --disable-lirc --enable-a52 --disable-aa --enable-dvbpsi --enable-mozilla --disable-kde --enable-mp4 --enable-dvb --disable-satellite --enable-ogg --enable-vorbis --disable-shout --disable-wxwidgets --enable-qt4 --disable-slp --enable-flac --disable-skins --disable-basic-skins --enable-skins2 --enable-freetype --enable-mkv --enable-speex --enable-caca --disable-live555 --enable-libmpeg2 --disable-fribidi --enable-cdio --enable-mod --enable-theora --enable-modplug  --enable-gnutls --enable-ffmpeg --enable-ncurses --enable-smb --disable-gnomevfs --enable-bonjour --enable-mpc --enable-vcd --enable-vcdx --enable-notify --enable-debug --enable-twolame --enable-x264 --enable-faad --disable-zvbi --enable-telx --enable-mediacontrol-bindings --disable-x264 --disable-atmo --enable-alsa --enable-dv --enable-v4l --enable-v4l2 --enable-pvr --enable-svgalib --disable-dvd --without-dvdcss
+make ; make install 
+#/opt/vlc/bin/vlc
